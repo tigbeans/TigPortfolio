c6baffdcdd77fa9a330c59db07f3eb44e0161616
austin <tigbeans@gmail.com>
2017-08-20 01:37:15 -0600

    created routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,32 @@ Rails.application.routes.draw do
 
   namespace :admins do
     get '/account' => 'accounts#edit', as: :account
+    get '/resume' => 'resume#edit', as: :resume
+    put '/info' => 'accounts#update_info', as: :info
+    put '/change_password' => 'accounts#change_password', as: :change_password
+
+    resources :posts
+    resources :projects
+    resources :resumes
+
   end
 
   scope module: 'all' do
+
+    # blog posts
+    scope module: 'blog' do
+      get 'posts' => 'posts#index', as: :posts
+      get 'posts' => 'posts#show', as: :post
+    end
+
+    #project roadmap
+    scope module: 'projects_roadmap' do
+      get 'projects' => 'projects#index', as: :projects
+      get 'projects/:id' => 'projects#show', as: :project
+    end
+
+    #resume
+    get 'resume' => 'resume#index', as: :resume
   end
 
 end

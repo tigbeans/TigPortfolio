bfb4d014dc87510005d55743314c3cf7856f28bd
Austin Cobbs <ajcobbs@yahoo.com>
2017-08-25 19:23:40 -0600

    posts index, seed posts in database, posts styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(title: "post one",
+            content: "Fixie mixtape viral lumbersexual tumblr squid authentic green juice tacos 8-bit bespoke aesthetic truffaut. Selfies ramps roof party, godard tbh chia pok pok air",
+            image_url: "https://images.pexels.com/photos/129105/pexels-photo-129105.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+Post.create(title: "post two",
+            content: "plant asymmetrical squid single-origin coffee tote bag ennui migas XOXO. Tumeric tattooed microdosing, raw denim la croix enamel pin glossier disrupt",
+            image_url: "https://images.pexels.com/photos/127723/pexels-photo-127723.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+Post.create(title: "post numero tres",
+            content: "kogi live-edge meh dreamcatcher. Shabby chic schlitz bushwick keffiyeh 8-bit hashtag banjo sartorial man braid fixie. Bitters snackwave selfies, fanny pack",
+            image_url: "https://images.pexels.com/photos/129612/pexels-photo-129612.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+Post.create(title: "four times a charm",
+            content: "master cleanse raclette tumeric asymmetrical. Shaman vinyl chia yuccie green juice mlkshk beard art party skateboard poke single-origin coffee selfies",
+            image_url: "https://images.pexels.com/photos/116876/pexels-photo-116876.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+Post.create(title: "wow fifth post",
+            content: "distillery. Occupy freegan chia meh messenger bag kitsch, copper mug master cleanse put a bird on it adaptogen neutra meggings. Locavore pug food truck",
+            image_url: "https://images.pexels.com/photos/126790/pexels-photo-126790.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+Post.create(title: "hey this is cool",
+            content: "shoreditch. Meggings chartreuse williamsburg swag salvia unicorn, bespoke fashion axe kickstarter mixtape succulents readymade. Vegan gochujang 8-bit",
+            image_url: "https://images.pexels.com/photos/117831/pexels-photo-117831.jpeg?h=350&auto=compress&cs=tinysrgb")
+
+Post.create(title: "i am also cool",
+            content: "shabby chic glossier franzen cred direct trade artisan fashion axe heirloom scenester. Tumblr quinoa mlkshk la croix XOXO wayfarers retro twee lyft fixie",
+            image_url: "https://images.pexels.com/photos/116651/pexels-photo-116651.png?h=350&auto=compress&cs=tinysrgb")
